5bd71763f72acdd78941d5423811f2602e76c871
Jon <jonlz@github.com>
2015-11-17 16:04:08 -0500

    Added pods and files

---
+++ DonateApp/DONMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DONMainViewController.h
+//  DonateApp
+//
+//  Created by Jon on 11/17/15.
+//  Copyright © 2015 Rolodex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DONMainViewController : UIViewController
+
+@end
---
+++ DonateApp/DONMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  DONMainViewController.m
+//  DonateApp
+//
+//  Created by Jon on 11/17/15.
+//  Copyright © 2015 Rolodex. All rights reserved.
+//
+
+#import "DONMainViewController.h"
+
+@interface DONMainViewController ()
+
+@end
+
+@implementation DONMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
