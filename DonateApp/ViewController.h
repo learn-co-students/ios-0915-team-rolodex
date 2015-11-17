822e5bdb5cba3bdd701a4841169234248580a92a
Jon <jonlz@github.com>
2015-11-17 15:45:24 -0500

    initial commit

---
+++ DonateApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DonateApp
+//
+//  Created by Jon on 11/17/15.
+//  Copyright © 2015 Rolodex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DonateApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DonateApp
+//
+//  Created by Jon on 11/17/15.
+//  Copyright © 2015 Rolodex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
