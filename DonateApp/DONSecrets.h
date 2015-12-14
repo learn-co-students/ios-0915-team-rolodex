a04714bad5aa03857ccf12a016275c6e7e78e445
synesthesio <msinger681@gmail.com>
2015-12-14 17:18:18 -0500

    ready to work

--- DonateApp/DONSecrets.h
+++ DonateApp/DONSecrets.h
@@ -1,16 +1,22 @@
 //
 //  DONSecrets.h
-//  DonateApp
+//  CURB ALERT
 //
-//  Created by Jon on 11/17/15.
+//  Created by synesthesia on 12/14/15.
 //  Copyright © 2015 Rolodex. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #ifndef DONSecrets_h
 #define DONSecrets_h
+
+@interface DONSecrets : NSObject
 
 extern NSString *const PARSE_APPLICATION_ID;
 extern NSString *const PARSE_CLIENT_KEY;
 extern NSString *const GOOGLE_MAPS_API_KEY;
 
 #endif /* DONSecrets_h */
+
+
+@end
---
+++ DonateApp/DONSecrets.m
@@ -0,0 +1,13 @@
+//
+//  DONSecrets.m
+//  CURB ALERT
+//
+//  Created by synesthesia on 12/14/15.
+//  Copyright © 2015 Rolodex. All rights reserved.
+//
+
+#import "DONSecrets.h"
+
+@implementation DONSecrets
+
+@end
