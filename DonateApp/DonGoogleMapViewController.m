87b65eab9631258fa0d9516c3ca922a4e245cc73
guangLess <bha.guang@gmail.com>
2016-01-28 13:02:40 -0500

    fix infobox thread/auto layout performance

--- DonateApp/DonGoogleMapViewController.m
+++ DonateApp/DonGoogleMapViewController.m
@@ -26,6 +26,7 @@
 //@property (weak, nonatomic) IBOutlet UIButton *backHomeButton;
 
 @property (strong, nonatomic) UIView * mView;
+@property (nonatomic, strong) GMSMarker *ignoreNextMarkerSelectionForThisMarker;
 
 @end
 
@@ -84,15 +85,18 @@
             NSLog(@"category name = %@ with image %@", category.name, category.imageFile);
             
             UIImage *localIconImage = [self imageByDrawingWhiteCircleBehindImage:[UIImage imageNamed:[NSString stringWithFormat:@"%@.png",category.name]] andResizingToSize:CGSizeMake(25, 25) withColor:[UIColor whiteColor]];
-
-            __weak typeof(self) tmpself = self;
-            [tmpself addMarkerWithItem:eachItem WithLatitude:eachItem.location.latitude longitude:eachItem.location.longitude discription:eachItem.itemDescription itemImage:localIconImage];
+            
+            //            __weak typeof(self) tmpself = self;
+            //            [tmpself addMarkerWithItem:eachItem WithLatitude:eachItem.location.latitude longitude:eachItem.location.longitude discription:eachItem.itemDescription itemImage:localIconImage];
+            //
+            
+            [self addMarkerWithItem:eachItem WithLatitude:eachItem.location.latitude longitude:eachItem.location.longitude discription:eachItem.itemDescription itemImage:localIconImage];
+            
             
             /*
              __block UIImage *iconImage = [UIImage new];
              PFFile *iconImageFile = category.imageFile;
              __weak typeof(self) tmpself = self;
-             
              //TODO: Create a weak reference to SELF to be used within this block (calling self in BLOCK created a ?retain cycle)
              */
             
@@ -138,12 +142,49 @@
 {
     NSLog(@"infoVieww!");
     
-    DonInfowindow * infoWindow = [[[NSBundle mainBundle] loadNibNamed:@"DonInfowindow" owner:self options:nil] objectAtIndex:0];
+   // DonInfowindow * infoWindow = [[[NSBundle mainBundle] loadNibNamed:@"DonInfowindow" owner:self options:nil] objectAtIndex:0];
     
     //[[NSOperationQueue mainQueue] addOperationWithBlock:^{
-    infoWindow.itemImage.file = [marker.userData imageFile];
-    [infoWindow.itemImage loadInBackground];
+    ///infoWindow.itemImage.file = [marker.userData imageFile];
+   // [infoWindow.itemImage loadInBackground];
     // }];
+    
+    
+    
+    
+    
+    
+    
+    DonInfowindow * infoWindow = [[[NSBundle mainBundle] loadNibNamed:@"DonInfowindow" owner:self options:nil] objectAtIndex:0];
+    
+        //[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+    infoWindow.itemImage.file = [marker.userData imageFile];		      infoWindow.itemImage.file = [marker.userData imageFile];
+       [infoWindow.itemImage loadInBackground];		
+     
+       // }];		
+       // So Google maps only renders the marker window once, when we return it from this function.
+       // If it changes after the fact (for instance, when the image loads), it does not get refreshed. :(
+       // SO... when the image comes down, we need to fake the user reselecting the marker (by setting mapView.selectedMarker)
+       // BUT... doing that triggers a call to this method again, which just leads to an infinite loop.
+       // So... we let that happen once, but use the ignoreNextMarkerSelectionForThisMarker property to make sure we don't
+       // loop forever.
+   
+   
+       [infoWindow.itemImage loadInBackground:^(UIImage * _Nullable image, NSError * _Nullable error) {
+       if(mapView.selectedMarker == marker) {  // make sure the user hasn't tapped a different marker in the meantime
+         if(self.ignoreNextMarkerSelectionForThisMarker != marker) {  // and we're not avoiding that infinite loop...
+                   self.ignoreNextMarkerSelectionForThisMarker = marker;    // set this so we only redraw once
+
+     [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                           mapView.selectedMarker = marker;                     // re-select the marker
+                       }];
+               }
+             }
+     }];
+    
+    
+    
+    // still trying to process this.
     
     UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
     UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:blur];
